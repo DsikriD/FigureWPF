b5d40aaef1b8f3ecafd7a6e799d4dc95cc7a3f4f
DsikriD <sikrierdanila@yandex.ru>
2024-01-16 19:49:15 +0300

    Добавьте файлы проекта.

---
+++ Figure/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Figure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbSetValue();
+        }
+
+       private Dictionary<string, MyShape> TypesFigure = new Dictionary<string, MyShape>
+       {
+            { "Квадрат", new MyRectangle()},
+            { "Круг", new MyElipse()},
+            { "Треугольник", new MyTriangular()},
+            { "Овал",new MyOval()}
+       };
+
+        private MyShape shape = new MyRectangle();
+
+        private void Draw()
+        {
+            Clear_Canvas();
+            cnv.Children.Add(shape.getFigure());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           Clear_Canvas();
+        }
+
+        public void Clear_Canvas() => cnv.Children.Clear();
+
+        public void cmbSetValue()
+        {
+            foreach (var type in TypesFigure)
+                cmb.Items.Add(type.Key);
+
+            cmb.SelectedIndex = 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            shape = (TypesFigure.Single(x=> x.Key == cmb.SelectedValue.ToString())).Value;
+        }
+    }
+}
---
+++ Figure/MyShape.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Figure
+{
+    
+
+    public abstract class MyShape : Shape
+    {
+        public abstract Shape getFigure();
+
+    }
+
+    public class MyElipse : MyShape
+    {
+        Ellipse elipse = new Ellipse();
+
+        protected override Geometry DefiningGeometry => elipse.RenderedGeometry;
+
+        public override Shape getFigure()
+        {
+            elipse.Height = 30;
+            elipse.Width = 30;
+            elipse.StrokeThickness = 1;
+            elipse.Fill = Brushes.Red;
+            elipse.Stroke = Brushes.Green;
+            return elipse;
+        }
+    }
+
+    public class MyRectangle : MyShape
+    {
+        Rectangle rect = new Rectangle();
+
+        protected override Geometry DefiningGeometry => rect.RenderedGeometry;
+
+        public override Shape getFigure()
+        {
+            rect.Height = 30;
+            rect.Width = 30;
+            rect.StrokeThickness = 1;
+            rect.Fill = Brushes.Red;
+            rect.Stroke = Brushes.Green;
+            return  rect;
+        }
+    }
+
+    public class MyTriangular : MyShape
+    {
+        Polygon trin = new Polygon();
+
+        protected override Geometry DefiningGeometry => trin.RenderedGeometry;
+
+        public override Shape getFigure()
+        {
+
+            trin.StrokeThickness = 1;
+            trin.Fill = Brushes.Red;
+            trin.Stroke = Brushes.Green;
+
+            var PointsSector = new PointCollection()
+            {
+                new Point(0,30),
+                new Point(30,0),
+                new Point(0,0),
+            };
+
+            trin.Points = PointsSector;
+            return trin;
+        }
+    }
+
+    public class MyOval : MyShape
+    {
+        Ellipse elipse = new Ellipse();
+
+        protected override Geometry DefiningGeometry => elipse.RenderedGeometry;
+
+        public override Shape getFigure()
+        {
+            elipse.Height = 30;
+            elipse.Width = 50;
+            elipse.StrokeThickness = 1;
+            elipse.Fill = Brushes.Red;
+            elipse.Stroke = Brushes.Green;
+            return elipse;
+        }
+
+    }
+
+
+
+}
